0330987f553c010bfffa4d1f22cad4cbe7433f2f
leticiariba <137909030+leticiariba@users.noreply.github.com>
2023-09-25 11:42:41 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+calcule.addEventListener("click", function () {
+  var a = valorA.value;
+  var b = valorB.value;
+  var op = operacao.value;
+  a = parseint(a);
+  b = parseint(b);
+  op = parseint(OP);
+  if (OP == 1) {
+    alert(a + b);
+    
+  else if (op == 2){
+    alert(a-b);
+
+  else if (op == 3){
+      alert(a°b);
+
+else if (op == 4){
+     alert(a/b);
+
+
+  }
+  
+});
